1a8d610a7923901a68130d763261ff5419da084f
jsimpsonux <jsimpsonux@gmail.com>
2024-01-13 19:35:06 +0000

    First commit, project setup

---
+++ assets/js/logic.js
@@ -0,0 +1,5 @@
+const startButton = document.getElementById('start');
+
+function displayQuestion(){
+    
+}
---
+++ assets/js/questions.js
@@ -0,0 +1,38 @@
+const quizQuestions = [
+    {
+        question: "Who's going to be the best Front end developer?",
+        answers: {
+            a: "...Me?",
+            b: "someone else",
+            c: "Probably someone at Google idk"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Who's going to smash this challenge and feel GREAT",
+        answers: {
+            a: "I don't know man it seems kinda hard",
+            b: "hopefully me",
+            c: "I will"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Who's smart and a hard worker?",
+        answers: {
+            a: "someone in class",
+            b: "That Elon guy is pretty smart",
+            c: "There can be no one else BUT ME"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "Who's not going to regret working an 8 hour job and studying in a front end developer bootcamp?!!",
+        answers: {
+            a: "*tears* *tears*",
+            b: "I'm feeling great!!",
+            c: "YES"
+        },
+        correctAnswer: "a"
+    }
+]
