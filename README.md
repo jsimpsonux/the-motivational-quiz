f4c903f76ffaed4d445099f8e1473f85bfae09d5
jsimpsonux <jsimpsonux@gmail.com>
2024-01-16 23:17:27 +0000

    Added README file and added comments to the logic file to explain the code

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # the-motivational-quiz
+JavaScript console finance challenge
+
+## Description
+
+A motivation quiz game using DOM manipulation and JavaScript, also allows for users to store highscores.
+
+## Table of Contents
+
+N/A
+
+## Installation
+
+N/A
+
+## Usage
+
+Users have a timer to beat and answer the questions correctly and effectively 
+they answer each question and are prompt with the next question but if they choose the incorrect answer the timer is supposed to lose 10 seconds as a penalty to the user.
+
+## Features
+
+JavaScript
+
+HTML
+
+DOM
+
+Functions
--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,4 @@
+// the group of const below select Id's in the HTML page and store in variable.
 const startButton = document.getElementById('start');
 const eachQuestion = document.getElementById('question-title');
 const answerChoices = document.getElementById('choices');
@@ -5,10 +6,12 @@ const timerElement = document.getElementById('time');
 const hideRemove = document.getElementById('questions');
 const startScreen = document.getElementById('start-screen')
 
-
+// variable for checking if user has won, begins as false. Timer variable to be used later in a function.
 var hasWon = false;
 var timer;
 var timerCount;
+
+// function timer is the countdown clock used in the quiz, and starts from a set time and counts downwards.
 
 function Timer() {
     timer = setInterval(function() {
@@ -28,8 +31,13 @@ function Timer() {
     }, 75 * 1000);
 }
 
+// Here is where the quizQuestions array in the questions page is stored, also the index to access the array.
+
 var currentQuestion = 0;
 var question = quizQuestions[currentQuestion];
+
+// function displayQuestions is the function is that displays each question and its options, it also accesses where the questions will be 
+// placed inside the HTML, console.log was added to test whether the browser is accessing inside the function.
 
 function displayQuestions(){
     console.log(question);
@@ -47,7 +55,13 @@ function displayQuestions(){
 
 // console.log(quizQuestions.answers[0]);
 
+// myScore stores the base value of the score which will increment the more correct answers the user gets. 
+
 var myScore = 0;
+
+// This function checks the correct answer that the users inputs, it takes the selectedAnswer and checks if it equals to the correct answer
+// stored inside the array in the quizQuestions array, it's supposed to match the string, then it will increase the score and change to th next question.
+// unless the user gets it wrong then it will remove time from the timer but still move to the nest question.
 
 function checkAnswer(selectedAnswer) {
     console.log("inside checkAnswer function");
@@ -71,6 +85,8 @@ function checkAnswer(selectedAnswer) {
     // }
 }
 
+// Here is the start function that kicks of the quiz game, I had to hide classes so that the questions could display.
+// when the event listener is activated at the bottom of this file, the display function and the timer should start automatically.
 
 function start(){
     hasWon = false;
